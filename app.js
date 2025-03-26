a1b9b9b3452d68d693d19a8217e76ea9c5b73857
Naomi0410 <naomialicia15@gmail.com>
2025-03-26 19:49:05 +0100

    cache

--- app.js
+++ app.js
@@ -79,6 +79,15 @@ const app = express();
 
 app.use(express.json()); // Body parser
 
+// Caching configuration
+app.use((req, res, next) => {
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.header("Pragma", "no-cache");
+  res.header("Expires", 0);
+  next();
+});
+
+
 // Routes
 app.use("/contact", contactRoutes);
 app.use("/subscribe", subscriberRoutes);
